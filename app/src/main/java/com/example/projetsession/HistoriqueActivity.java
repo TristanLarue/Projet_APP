f694e930f7d5b7c6acdcea689c2b7b1d1b741fd4
KahinaMedkour <91035597+KahinaMedkour@users.noreply.github.com>
2025-04-04 13:08:05 -0400

    app

---
+++ app/src/main/java/com/example/projetsession/HistoriqueActivity.java
@@ -0,0 +1,24 @@
+package com.example.projetsession;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HistoriqueActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_historique);
+
+        // Gérer la StatusBar pour qu'elle n'empiète pas sur le contenu
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            getWindow().setDecorFitsSystemWindows(true);
+        } else {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+    }
+}
